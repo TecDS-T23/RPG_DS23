951ebd78034118cc618b43cfe2fe50e1c95a559f
viniciuss19 <79714215+viniciuss19@users.noreply.github.com>
2021-03-04 15:28:23 -0300

    Add files via upload

---
+++ Bardo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_DS23
+{
+    class Bardo
+    {
+        string nome;
+        int forca;
+        int destreza;
+        int velocidade;
+    }
+}
+
