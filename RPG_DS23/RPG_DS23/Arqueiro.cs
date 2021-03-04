41922d40fbdb17577648c15cd2ace444cdcff270
fernandonakagawa <fernando.nakagawa@sesisenaipr.org.br>
2021-03-04 14:30:35 -0300

    Criação do projeto

---
+++ RPG_DS23/RPG_DS23/Arqueiro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_DS23
+{
+    class Arqueiro
+    {
+        string nome;
+        int forca;
+        int destreza;
+        int velocidade;
+    }
+}
---
+++ RPG_DS23/RPG_DS23/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPG_DS23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RPG DS23!");
+        }
+    }
+}
