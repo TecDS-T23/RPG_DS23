7376f8d2b8caacd79488f9b2af1962bf5b445177
VituhPalmituh <vitorferreiradsilva@gmail.com>
2021-03-04 14:48:33 -0300

    Criação assassino

---
+++ RPG_DS23/RPG_DS23/Assassino.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_DS23
+{
+    class Assassino
+    {
+        string nome;
+        int forca;
+        int destreza;
+        int velocidade;
+    }
+}
