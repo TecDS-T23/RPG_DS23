a70be1e9cec1891860246a3dce04459b3b2f7e9f
Arrezeb <78730877+Arrezeb@users.noreply.github.com>
2021-03-05 19:31:44 -0300

    Add files via upload

---
+++ Mago.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_DS23
+{
+    /**
+     * Mago ataca a distância  
+    */
+    class Mago
+    {
+        string nome;
+        int força;
+        int velocidade;
+        int vida;
+        int destreza;
+    }
+}
